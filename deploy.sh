b4288b3e9b0b824a1f83a18944819cdffd53d290
josel1204 <josel1204@gmail.com>
2021-06-15 17:40:26 -0700

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+ docker build -t josel1204/multi-client:latest -t josel1204/multi-client:$SHA -f ./client/Dockerfile ./client
+ docker build -t josel1204/multi-server:latest -t josel1204/multi-server:$SHA -f ./server/Dockerfile ./server
+ docker build -t josel1204/multi-worker:latest -t josel1204/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push josel1204/multi-client:latest
+docker push josel1204/multi-server:latest
+docker push josel1204/multi-worker:latest
+
+docker push josel1204/multi-client:$SHA
+docker push josel1204/multi-server:$SHA
+docker push josel1204/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=josel1204/multi-server:$SHA
+kubectl set image deployments/client-deployment server=josel1204/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=josel1204/multi-worker:$SHA
